858a8131aba6e2acc381ca6ac10711ec6ead29f6
Mitesh Kumar Bajaj <miteshbajaj12@gmail.com>
2026-08-31 23:06:02 +0530

    constant sliding window

---
+++ Array/ConstantWindow.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {2, 1, 5, 1, 3, 2};
+    int n = 6;
+    int k = 3;
+
+    int windowSum = 0;
+
+    // First window
+    for (int i = 0; i < k; i++) {
+        windowSum += arr[i];
+    }
+
+    int maxSum = windowSum;
+
+    // Slide the window
+    for (int i = k; i < n; i++) {
+        windowSum += arr[i];
+        windowSum -= arr[i - k];
+
+        maxSum = max(maxSum, windowSum);
+    }
+
+    cout << "Maximum sum: " << maxSum << endl;
+
+    return 0;
+}
