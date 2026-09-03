ebd44649038852551a2de2ee0a291eddc17806d7
Mitesh Kumar Bajaj <miteshbajaj12@gmail.com>
2026-09-03 14:41:44 +0530

    kadane algorithm

---
+++ Array/Kadane.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int arr[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    int n = 9;
+
+    int currentSum = arr[0];
+    int maxSum = arr[0];
+
+    for (int i = 1; i < n; i++) {
+
+        if (currentSum + arr[i] > arr[i])
+            currentSum += arr[i];
+        else
+            currentSum = arr[i];
+
+        if (currentSum > maxSum)
+            maxSum = currentSum;
+    }
+
+    cout << "Maximum subarray sum: " << maxSum << endl;
+
+    return 0;
+}
